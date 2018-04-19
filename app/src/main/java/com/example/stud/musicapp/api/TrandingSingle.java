c31a44708eae1287448ba746bb7ac084e1b0cca7
w57020 <zaq1@WSX>
2018-04-19 18:09:42 +0200

    Modele

---
+++ app/src/main/java/com/example/stud/musicapp/api/TrandingSingle.java
@@ -0,0 +1,18 @@
+package com.example.stud.musicapp.api;
+
+/**
+ * Created by W57020 on 2018-04-19.
+ */
+
+public class TrandingSingle {
+    public int idAlbum;
+    public int idArtist;
+    public int idTrack;
+    public int idTrend;
+    public int intChartPlace;
+    public String strAlbum;
+    public String strArtist;
+    public String strTrack;
+    public String strTrackThumb;
+
+}
---
+++ app/src/main/java/com/example/stud/musicapp/api/TrendingList.java
@@ -0,0 +1,14 @@
+package com.example.stud.musicapp.api;
+
+import java.util.List;
+
+/**
+ * Created by W57020 on 2018-04-19.
+ */
+
+public class TrendingList {
+
+    public List<TrandingSingle> trending;
+    
+
+}
