a4ddc7c0fff4414e2699f59201e475f1d9db9a4b
W57020 <mryczus@gmail.com>
2018-05-24 18:58:57 +0200

    Szablon wyszukiwarki

--- app/src/main/java/com/example/stud/musicapp/searchalbum/SearchAlbumActivity.java
+++ app/src/main/java/com/example/stud/musicapp/searchalbum/SearchAlbumActivity.java
@@ -1,27 +1,63 @@
 package com.example.stud.musicapp.searchalbum;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.stud.musicapp.MainActivity;
 import com.example.stud.musicapp.R;
 import com.example.stud.musicapp.favorites.FavoritesActivity;
 
 public class SearchAlbumActivity extends AppCompatActivity {
+EditText etQuery;
+RecyclerView rvList;
+
+
+SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_album);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+
+
+        etQuery = findViewById(R.id.etQuery);
+                rvList = findViewById(R.id.rvList);
+
+        String artist = sharedPreferences.getString("query",null);
+        etQuery.setText(artist);
+
+        Button bSearch = findViewById(R.id.bSearch);
+        bSearch.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+            String query = etQuery.getText().toString();
+            rememberQuery(query);
+            }
+        });
+
+
+    }
+    private void rememberQuery(String query) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("query",query);
+        editor.apply();
+
+
     }
 
-
-
-
-
+public boolean onSupportNabigateUp(){
+        onBackPressed();
+        return true;
+}
 
 
 }
